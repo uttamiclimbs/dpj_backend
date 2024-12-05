7f203a0094b5f97931f635e6075863e26edd4dff
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-12-05 11:02:18 +0530

    Created Model For Spearker

---
+++ model/speakser.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const speakerSchema = mongoose.Schema({
+    eventId: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: true
+    },
+    CreatedAt: { type: Date, default: Date.now }, // Save The Time When the following Job was created 
+})
