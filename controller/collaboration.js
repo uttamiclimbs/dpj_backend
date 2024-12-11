d2b1afcbf03dea30e63f95e5776920feebed3e68
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-12-11 13:14:05 +0530

    Uploading New Commit -m

--- controller/collaboration.js
+++ controller/collaboration.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const jwt = require('jsonwebtoken');
 const { EventModel } = require("../model/event.model");
 const { ArtistAuthentication } = require("../middleware/Authentication");
 const { CollabModel } = require("../model/collaboration.model");
