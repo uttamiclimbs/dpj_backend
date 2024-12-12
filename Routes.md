08fb1cb66474c40b7bebb8cf605369cae3019805
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-12-12 11:34:03 +0530

    Added Collaborators Routest in Routes.md File

--- Routes.md
+++ Routes.md
@@ -1,78 +1,102 @@
-# Collaboration Routes :- 
-# Creating Collaboration Between Artists 
+# Collaboration Routes :-
+
+# Creating Collaboration Between Artists
 
 url :- http://localhost:4500/api/v1/collaborator/add
 
 Payload :- {
- "title":"Testing",
-  "description":"Testing",
-  "category":"Category Testing",
-  "banner":File Uploding,
-  "startDate":"2024-12-06",
-  "endDate":"2024-12-12", 
-  "startTime":"05:00",
-  "endTime":"11:00",
-  "collaborators":[
-    {
-      id:Candidate Id,
-      name:"Testing",
-      email:"testing@gmail.com",
-      amount:2500
-    }
-  ] 
+"title":"Testing",
+"description":"Testing",
+"category":"Category Testing",
+"banner":File Uploding,
+"startDate":"2024-12-06",
+"endDate":"2024-12-12",
+"startTime":"05:00",
+"endTime":"11:00",
 }
 
 # Response From Server
+
 For Success
 {
-  "status": "success",
-  "message": "Registration Successful",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzI4N2U0ZTY3MjA3NDBjNThiZDkyYjMiLCJuYW1lIjoiVXR0YW0gS3VtYXIgU2hhdyIsImVtYWlsIjoidXR0YW1rcnNoYXdAaWNsaW1icy5jb20iLCJleHAiOjE3MzA3MzIyMjIsImlhdCI6MTczMDcwNzAyMn0.KPytgnSBjErwinogDAgrll34QcD5Tx2uOrPP3dMT5DU"
+"status": "success",
+message: `Collaboration Created Successfully`,
 }
 
 For Error
 {
-  "status": "error",
-  "message": "User Already Exists with this Email ID. Please Try again with another Email ID",
-  "redirect": "/user/login"
+"status": "error",
+message: `Failed To Add New Event ${error.message}`,
 }
 
+# Creating Artists As Collaboration
 
+url :- http://localhost:4500/api/v1/collaborator/add/collaborator/:id
 
-# Editing Collaboration Between Artists 
+Payload :-{
+"collaborators":[
+{
+id:Candidate Id,
+name:"Testing",
+email:"testing@gmail.com",
+amount:2500
+},
+{
+id:Candidate Id,
+name:"Testing",
+email:"testing@gmail.com",
+amount:2500
+}
+]
+}
+
+# Response From Server
+
+For Success
+{
+"status": "success",
+"message": "Successfully Added Collaborators in The Following Collaboration Event",
+}
+
+For Error
+{
+"status": "error",
+"message": "Failed To Add New Event ${error.message}",
+}
+
+# Editing Collaboration Between Artists
 
 url :- http://localhost:4500/api/v1/collaborator/edit/basic/:id
 
 const { id } = req.params;
 
 Payload :- {
- "title":"Testing",
-  "description":"Testing",
-  "category":"Category Testing",
-  "banner":File Uploding,
-  "startDate":"2024-12-06",
-  "endDate":"2024-12-12", 
-  "startTime":"05:00",
-  "endTime":"11:00",
-  "banner":fileupload
+"title":"Testing",
+"description":"Testing",
+"category":"Category Testing",
+"banner":File Uploding,
+"startDate":"2024-12-06",
+"endDate":"2024-12-12",
+"startTime":"05:00",
+"endTime":"11:00",
+"banner":fileupload
 }
 
 # Response From Server
+
 For Success
 {
-  "status": "success",
-  "message": "Registration Successful",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzI4N2U0ZTY3MjA3NDBjNThiZDkyYjMiLCJuYW1lIjoiVXR0YW0gS3VtYXIgU2hhdyIsImVtYWlsIjoidXR0YW1rcnNoYXdAaWNsaW1icy5jb20iLCJleHAiOjE3MzA3MzIyMjIsImlhdCI6MTczMDcwNzAyMn0.KPytgnSBjErwinogDAgrll34QcD5Tx2uOrPP3dMT5DU"
+"status": "success",
+"message": "Registration Successful",
+"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzI4N2U0ZTY3MjA3NDBjNThiZDkyYjMiLCJuYW1lIjoiVXR0YW0gS3VtYXIgU2hhdyIsImVtYWlsIjoidXR0YW1rcnNoYXdAaWNsaW1icy5jb20iLCJleHAiOjE3MzA3MzIyMjIsImlhdCI6MTczMDcwNzAyMn0.KPytgnSBjErwinogDAgrll34QcD5Tx2uOrPP3dMT5DU"
 }
 
 For Error
 {
-  "status": "error",
-  "message": "User Already Exists with this Email ID. Please Try again with another Email ID",
-  "redirect": "/user/login"
+"status": "error",
+"message": "User Already Exists with this Email ID. Please Try again with another Email ID",
+"redirect": "/user/login"
 }
-
-
 
 address: "Create your physical Collaboration by providing the details below"
 banner: File {name: 'Rectangle 317.png', lastModified: 1733489274041, lastModifiedDate: Fri Dec 06 2024 18:17:54 GMT+0530 (India Standard Time), webkitRelativePath: '', size: 85798, …}
--- controller/collaboration.js
+++ controller/collaboration.js
@@ -9,158 +9,158 @@ const CollabRouter = express.Router();
 const uploadPath = path.join(__dirname, "../public/collaborations");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadPath);
-    },
-    filename: function (req, file, cb) {
-        let uniqueSuffix = Date.now();
-        cb(null, uniqueSuffix + file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    let uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 CollabRouter.post(
-    "/add",
-    upload.single("banner"),
-    ArtistAuthentication,
-    async (req, res) => {
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, "Authentication");
-        const fileName = req.file.filename;
-        const {
-            title,
-            description,
-            address,
-            eventType,
-            category,
-            startDate,
-            endDate,
-            startTime,
-            endTime,
-            collaborators,
-        } = req.body;
-        const startDateTime = new Date(`${startDate}T${startTime}`);
-        const endDateTime = new Date(`${endDate}T${endTime}`);
-        const collaboration = new EventModel({
-            address: address,
-            title: title,
-            banner: fileName,
-            description: description,
-            category: category,
-            startDateTime: startDateTime,
-            endDateTime: endDateTime,
-            eventType: eventType,
-            type: "Collaboration",
-            createdBy: decoded._id,
-        });
-        try {
-            savedDocument = await collaboration.save();
-            res.json({
-                status: "success",
-                message: `Collaboration Created Successfully`,
-            });
-        } catch (error) {
-            res.json({
-                status: "error",
-                message: `Failed To Add New Event ${error.message}`,
-            });
-        }
+  "/add",
+  upload.single("banner"),
+  ArtistAuthentication,
+  async (req, res) => {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, "Authentication");
+    const fileName = req.file.filename;
+    const {
+      title,
+      description,
+      address,
+      eventType,
+      category,
+      startDate,
+      endDate,
+      startTime,
+      endTime,
+      collaborators,
+    } = req.body;
+    const startDateTime = new Date(`${startDate}T${startTime}`);
+    const endDateTime = new Date(`${endDate}T${endTime}`);
+    const collaboration = new EventModel({
+      address: address,
+      title: title,
+      banner: fileName,
+      description: description,
+      category: category,
+      startDateTime: startDateTime,
+      endDateTime: endDateTime,
+      eventType: eventType,
+      type: "Collaboration",
+      createdBy: decoded._id,
+    });
+    try {
+      savedDocument = await collaboration.save();
+      res.json({
+        status: "success",
+        message: `Collaboration Created Successfully`,
+      });
+    } catch (error) {
+      res.json({
+        status: "error",
+        message: `Failed To Add New Event ${error.message}`,
+      });
     }
+  }
 );
 
 CollabRouter.post(
-    "/add/collaborators/:id",
-    ArtistAuthentication,
-    async (req, res) => {
-        const { id } = req.params;
-        const { collaborators } = req.body;
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, "Authentication");
-
-        let addCollaborators = [];
-
-        for (let index = 0; index < collaborators.length; index++) {
-            addCollaborators.push({
-                userId: collaborators[index]._id,
-                email: collaborators[index].email,
-                name: collaborators[index].name,
-                amount: collaborators[index].amount,
-                eventId: id,
-            });
-        }
-        try {
-            const result = await CollabModel.insertMany(addCollaborators);
-            res.json({status:"success",message:'Successfully Added Collaborators in The Following Collaboration Event'})
-        } catch (error) {
-            res.json({
-                status: "error",
-                message: `Failed To Add New Event ${error.message}`,
-            });
-        }
+  "/add/collaborators/:id",
+  ArtistAuthentication,
+  async (req, res) => {
+    const { id } = req.params;
+    const { collaborators } = req.body;
+    let addCollaborators = [];
+    for (let index = 0; index < collaborators.length; index++) {
+      addCollaborators.push({
+        userId: collaborators[index]._id,
+        email: collaborators[index].email,
+        name: collaborators[index].name,
+        amount: collaborators[index].amount,
+        eventId: id,
+      });
     }
+    try {
+      const result = await CollabModel.insertMany(addCollaborators);
+      res.json({
+        status: "success",
+        message:
+          "Successfully Added Collaborators in The Following Collaboration Event",
+      });
+    } catch (error) {
+      res.json({
+        status: "error",
+        message: `Failed To Add New Event ${error.message}`,
+      });
+    }
+  }
 );
 
 CollabRouter.post(
-    "/edit/basic/:id",
-    ArtistAuthentication,
-    upload.single("banner"),
-    async (req, res) => {
-        const { id } = req.params;
-        const token = req.headers.authorization.split(" ")[1];
-        const {
-            title,
-            description,
-            category,
-            banner,
-            startDate,
-            endDate,
-            startTime,
-            endTime,
-        } = req.body;
-        const decoded = jwt.verify(token, "Authentication");
-        const fileName = req.file.filename;
-        try {
-            const details = await EventModel.find({ _id: id });
-            details[0].title = title;
-            details[0].description = description;
-            details[0].category = category;
-            details[0].startDate = startDate;
-            details[0].endDate = endDate;
-            details[0].startTime = startTime;
-            details[0].endTime = endTime;
-            details[0].banner = fileName;
+  "/edit/basic/:id",
+  ArtistAuthentication,
+  upload.single("banner"),
+  async (req, res) => {
+    const { id } = req.params;
+    const token = req.headers.authorization.split(" ")[1];
+    const {
+      title,
+      description,
+      category,
+      banner,
+      startDate,
+      endDate,
+      startTime,
+      endTime,
+    } = req.body;
+    const decoded = jwt.verify(token, "Authentication");
+    const fileName = req.file.filename;
+    try {
+      const details = await EventModel.find({ _id: id });
+      details[0].title = title;
+      details[0].description = description;
+      details[0].category = category;
+      details[0].startDate = startDate;
+      details[0].endDate = endDate;
+      details[0].startTime = startTime;
+      details[0].endTime = endTime;
+      details[0].banner = fileName;
 
-            await details[0].save();
-            res.json({ status: "status", message: `Event Successfully Updatec` });
-        } catch (error) {
-            res.json({
-                status: "error",
-                message: `Failed To Add New Event ${error.message}`,
-            });
-        }
+      await details[0].save();
+      res.json({ status: "status", message: `Event Successfully Updatec` });
+    } catch (error) {
+      res.json({
+        status: "error",
+        message: `Failed To Add New Event ${error.message}`,
+      });
     }
+  }
 );
 
 CollabRouter.post(
-    "/edit/collaborators/:id",
-    ArtistAuthentication,
-    async (req, res) => {
-        const { id } = req.params;
-        const token = req.headers.authorization.split(" ")[1];
-        const { collaborators } = req.body;
-        const decoded = jwt.verify(token, "Authentication");
-        const fileName = req.file.filename;
-        try {
-            const details = await EventModel.find({ eventId: id });
-            res.json({ status: "status", message: `Event Successfully Updated` });
-        } catch (error) {
-            res.json({
-                status: "error",
-                message: `Failed To Add New Event ${error.message}`,
-            });
-        }
+  "/edit/collaborators/:id",
+  ArtistAuthentication,
+  async (req, res) => {
+    const { id } = req.params;
+    const token = req.headers.authorization.split(" ")[1];
+    const { collaborators } = req.body;
+    const decoded = jwt.verify(token, "Authentication");
+    const fileName = req.file.filename;
+    try {
+      const details = await EventModel.find({ eventId: id });
+      res.json({ status: "status", message: `Event Successfully Updated` });
+    } catch (error) {
+      res.json({
+        status: "error",
+        message: `Failed To Add New Event ${error.message}`,
+      });
     }
+  }
 );
 
 module.exports = { CollabRouter };
