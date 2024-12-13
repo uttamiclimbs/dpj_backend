b1e75fca6056ff939cef10214d89e2dffa42715e
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-12-13 12:17:38 +0530

    Completed Edit File Changes

--- controller/collaboration.js
+++ controller/collaboration.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("node:path");
+const fs = require('fs');
 const jwt = require("jsonwebtoken");
 const { EventModel } = require("../model/event.model");
 const { ArtistAuthentication } = require("../middleware/Authentication");
@@ -34,7 +35,6 @@ CollabRouter.post("/add", upload.single("banner"), ArtistAuthentication, async (
     endDate,
     startTime,
     endTime,
-    collaborators,
   } = req.body;
   const startDateTime = new Date(`${startDate}T${startTime}`);
   const endDateTime = new Date(`${endDate}T${endTime}`);
@@ -96,36 +96,33 @@ CollabRouter.post("/add/collaborators/:id", ArtistAuthentication, async (req, re
 
 CollabRouter.post("/edit/basic/:id", ArtistAuthentication, upload.single("banner"), async (req, res) => {
   const { id } = req.params;
-  const token = req.headers.authorization.split(" ")[1];
-  const {
-    title,
-    description,
-    category,
-    banner,
-    startDate,
-    endDate,
-    startTime,
-    endTime,
-  } = req.body;
-  const decoded = jwt.verify(token, "Authentication");
+  const { title, description, address, category, startDate, endDate, startTime, endTime, } = req.body;
+  const startDateTime = new Date(`${startDate}T${startTime}`);
+  const endDateTime = new Date(`${endDate}T${endTime}`);
   const fileName = req.file.filename;
   try {
     const details = await EventModel.find({ _id: id });
+    fs.unlink(`${uploadPath}/${details[0].banner}`, (err) => {
+      if (err) {
+        console.error('Error deleting old file:', err);
+      } else {
+        console.log('Old file deleted successfully');
+      }
+    });
     details[0].title = title;
     details[0].description = description;
     details[0].category = category;
-    details[0].startDate = startDate;
-    details[0].endDate = endDate;
-    details[0].startTime = startTime;
-    details[0].endTime = endTime;
+    details[0].startDateTime = startDateTime;
+    details[0].endDateTime = endDateTime;
     details[0].banner = fileName;
-
+    details[0].category = category;
+    details[0].address = address;
     await details[0].save();
     res.json({ status: "status", message: `Event Successfully Updatec` });
   } catch (error) {
     res.json({
       status: "error",
-      message: `Failed To Add New Event ${error.message}`,
+      message: `Failed To Edit Event Details ${error.message}`,
     });
   }
 }
